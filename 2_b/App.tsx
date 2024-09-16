4b9a08cfbf8ea30d5728d2b9269fcda6990ee5ac
quyentrann <dangthiquyentran22@gmail.com>
2024-09-16 23:54:23 +0700

    screen 2_b

--- 2_b/App.tsx
+++ 2_b/App.tsx
@@ -1,22 +1,129 @@
 import React from 'react';
-import { Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+
 const YourApp = () => {
   return (
     <View
       style={{
+        display: 'flex',
         flex: 1,
-        paddingHorizontal: 20,
+        justifyContent: 'center',
       }}>
-      <View style={{flex: 2}}>
-        <View style={{ flexDirection: 'row', flex: 1, marginTop: 10 }}>
-          <Image source={require('./image/usb.png')} />
-          <Text style={{ fontWeight: 'bold', paddingLeft: 5 }}>
-            USB Bluetooth Music Receiver HJX-001- Biến loa thường thành loa
-            bluetooth
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginLeft: 15,
+          alignItems: 'center',
+        }}>
+        <Image source={require('./image/usb.png')} />
+
+        <Text
+          style={{
+            fontWeight: 'bold',
+            textAlign: 'center',
+            alignItems: 'center',
+          }}>
+          USB Bluetooth Music Receiver HJX-001- Biến loa thường thành loa
+          bluetooth
+        </Text>
+      </View>
+
+      <View style={{ flex: 0.5, justifyContent: 'space-around' }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}>
+          Cực kỳ hài lòng
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginHorizontal: 40,
+            display: 'flex',
+          }}>
+          <Image source={require('./image/Star1.png')} />
+          <Image source={require('./image/Star1.png')} />
+          <Image source={require('./image/Star1.png')} />
+          <Image source={require('./image/Star1.png')} />
+          <Image source={require('./image/Star1.png')} />
+        </View>
+      </View>
+      <View style={{ flex: 0.6, justifyContent:'center'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <TouchableOpacity
+            style={{
+              height: 60,
+              width: 290,
+              borderColor: '#1511EB',
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 6,
+            }}>
+            <Text>Thêm hình ảnh</Text>
+          </TouchableOpacity>
+          <Image
+            source={require('./image/camera.png')}
+            style={{ position: 'absolute', marginRight: 150, display: 'flex' }}
+          />
+        </View>
+      </View>
+      <View style={{ flex: 1.8, alignItems: 'center' }}>
+        <TextInput
+          style={{
+            height: 250,
+            width: 290,
+            borderWidth: 1,
+            borderColor: '#bfbfbf',
+            fontWeight: 'bold',
+            color: '#bfbfbf',
+            padding: 15,
+            borderRadius: 6,
+          }}
+          placeholder="Hãy chi sẻ những điều mà bạn thích về sản phẩm"
+          multiline={true}
+        />
+        <Text
+          style={{
+            position: 'absolute',
+            fontWeight: 'bold',
+            marginTop: 220,
+            marginLeft: 70,
+            fontSize: 10,
+            alignItems: 'flex-end',
+          }}>
+          https://meet.google.com/nsj-ojwi-xpp
+        </Text>
+      </View>
+      <View style={{ alignItems: 'center', flex: 1 }}>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 290,
+            backgroundColor: '#0D5DB6',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+          }}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Gửi
           </Text>
-        </View>
-        <View>
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
